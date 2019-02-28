bd2b915d5d4dae9098372fc8f852e9cbb095b6bc
JasonChinsen <42547035+JasonChinsen@users.noreply.github.com>
2019-02-28 11:04:29 -0500

    initial Banzai Cloud Pipeline commit

---
+++ internal/greeting/greetingadapter/sayhello_events_test.go
@@ -0,0 +1,29 @@
+package greetingadapter
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/sagikazarmark/modern-go-application/internal/greeting"
+)
+
+func TestSayHelloEvents_SaidHelloTo(t *testing.T) {
+	publisher := &publisherStub{}
+
+	events := NewSayHelloEvents(publisher)
+
+	event := greeting.SaidHelloTo{
+		Message: "Hello, John!",
+		Who:     "John",
+	}
+
+	err := events.SaidHelloTo(context.Background(), event)
+	require.NoError(t, err)
+
+	assert.Equal(t, saidHelloToTopic, "said_hello_to")
+	assert.Equal(t, saidHelloToTopic, publisher.topic)
+	assert.Equal(t, string(publisher.messages[0].Payload), "{\"Message\":\"Hello, John!\",\"Who\":\"John\"}")
+}
---
+++ internal/greetingworker/sayhello_event_logger_test.go
@@ -0,0 +1,33 @@
+package greetingworker_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/goph/logur"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	. "github.com/sagikazarmark/modern-go-application/internal/greetingworker"
+	"github.com/sagikazarmark/modern-go-application/internal/greetingworker/greetingworkeradapter"
+)
+
+func TestSayHelloEventLogger_SaidHelloTo(t *testing.T) {
+	logger := logur.NewTestLogger()
+
+	eventLogger := NewSayHelloEventLogger(greetingworkeradapter.NewLogger(logger))
+
+	event := SaidHelloTo{
+		Message: "Hello, World!",
+		Who:     "John",
+	}
+
+	err := eventLogger.SaidHelloTo(context.Background(), event)
+	require.NoError(t, err)
+
+	lastLogEvent := logger.LastEvent()
+	assert.Equal(t, "said hello to someone", lastLogEvent.Line)
+	assert.Equal(t, logur.Info, lastLogEvent.Level)
+	assert.Equal(t, event.Message, lastLogEvent.Fields["message"])
+	assert.Equal(t, event.Who, lastLogEvent.Fields["who"])
+}
